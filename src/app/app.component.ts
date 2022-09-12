143e83d9b1700101e4083663291849fa60d18508
Robert Seidler <r.seidler@protronic-gmbh.de>
2022-09-12 13:44:20 +0200

    changed imports to npm published version; added locales and buildconfig for locales;

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 
+import { CalcType } from 'pro-connection-services';
+
+import { faProjectDiagram, faChartLine, faBullseye, faUnlock, faSpinner, faArrowAltCircleDown } from '@fortawesome/free-solid-svg-icons';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +11,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ang-prot-component-library-example';
+  
+  faProjectDiagram = faProjectDiagram;
+  faChartLine = faChartLine;
+  faBullseye = faBullseye;
+  faUnlock = faUnlock;
+  faSpinner = faSpinner;
+  faArrowAltCircleDown = faArrowAltCircleDown;
+
+  calcType = CalcType;
+  value: number = 0;
+
+  eingangNumber = 1;
+
+  itemLabel1:string = $localize`Eingang ${this.eingangNumber}`;
+
+  itemLablesM019:Array<string> = [
+    $localize`Einzelsensor`,
+    $localize`Durschnittswert`
+  ]
+
+  itemLablesMO230:Array<string> = [
+    $localize`Aus`,
+    $localize`Schließer`,
+    $localize`Öffner`
+  ]
+
+  itemLablesMO130:Array<string> = [
+    $localize`ausschalten`, 
+    $localize`auf Minimum Dimm`
+  ]
+
+  constructor(){}
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+
+import { ProConnectionServicesModule } from 'pro-connection-services';
+import { ProDimmSliderModule } from 'pro-dimm-slider';
+import { ProSelectModule } from 'pro-select-component';
+import { ProSliderModule } from 'pro-slider';
+
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ProConnectionServicesModule,
+    ProDimmSliderModule,
+    ProSelectModule,
+    ProSliderModule,
+    FontAwesomeModule
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
